7bd234946ec5263a3bee66dd3f08924eec8c450e
Joseph Ospina Tafur <josh.999126@gmail.com>
2019-06-01 14:28:05 -0500

    add tailwindcss

---
+++ dashboard-centinelbot/tailwind.config.js
@@ -0,0 +1,11 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://next.tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/next/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {},
+  variants: {},
+  plugins: []
+}
